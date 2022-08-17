c5454d6a5ea383fcdca5680cacb9d72bf56a84a3
probro27 <prabhavkhera@hotmail.com>
2022-08-16 21:40:58 -0400

    trying to fix bug

--- backend.js
+++ backend.js
@@ -2,7 +2,9 @@
 const express = require('express'); 
 const cors = require('cors'); 
 const fetch = require('node-fetch');
-const cheerio = require('cheerio');
+// const cheerio = require('cheerio');
+// const jsdom = require("jsdom");
+var DOMParser = require('xmldom').DOMParser;
 
 async function getTitle(url) {
 //   // get html text from reddit
